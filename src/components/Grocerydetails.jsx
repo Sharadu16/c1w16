96d45c6d65aca8e96f438d841775feeae69e8671
Sharadinduprakash <Sharadyadav2786@gmail.com>
2022-04-11 12:35:11 +0530

    correction

--- src/components/Grocerydetails.jsx
+++ src/components/Grocerydetails.jsx
@@ -1,15 +1,38 @@
 import React from "react"
 import data from '../data.json'
 
-
 const GroceryDetails = ()=>{
 console.log(data)
+  
+  data.map = (elem) => {
+     
+      let div = document.createElement("div");
 
+      let discount = document.createElement("h3");
+      discount.innerHTML = elem.discount;
+
+      let img = document.createElement("img");
+      img.src = elem.imgURL;
+      
+      let title = document.createElement("h4")
+      title.innerText= elem.title;
+
+      let price = document.createElement("h4");
+      price.innerText = elem.sellingPrice; 
+
+      let stackPrice = document.createElement("p");
+      stackPrice.innerText = elem.mrp;
+
+      div.append(discount, img, title, price, stackPrice);
+      document.querySelector(".container").append(div);
+     
+  }
+  
     return(
         <>
-        <h1>Groceries</h1>
+        <h1 className="heading">Groceries</h1>
         <div className="container">
-            {/* map through the data and display the cards */}
+            
         </div>
         </>
     )
